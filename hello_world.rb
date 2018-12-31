ab25b7a78deb71560d5fc5f789c0556649a73cec
Becca Elenzil <becca.elenzil@bush.edu>
2018-12-31 09:45:12 -0800

    practice with user input

---
+++ hello_world.rb
@@ -0,0 +1,4 @@
+puts "Hello, World!"
+
+#puts adds a new line after executing
+#prints does not
---
+++ programming-grammar.rb
@@ -0,0 +1,9 @@
+
+# Print to the terminal asking the user to enter their name
+puts "What's your name?"
+# Let's say that the user entered "Ada Lovelace"
+# The value of "Ada Lovelace/n" gets stored in the variable user_input
+user_input = gets
+# The value of "Ada Lovelace" gets stored in the variable name
+name = user_input.chomp
+puts name
